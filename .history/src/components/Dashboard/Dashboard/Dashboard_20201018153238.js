a2fb00bc1a3754125ae20a651ee5051449d02429
Your NameMdTahsinAmin <Tahsin16@cse.pstu.ac.bd.com>
2020-10-18 15:35:53 +0600

    all api change

---
+++ .history/src/components/Dashboard/Dashboard/Dashboard_20201018153238.js
@@ -0,0 +1,24 @@
+import React, { useContext, useEffect, useState } from 'react';
+import SideBar from './SideBar/SideBar';
+import { useForm } from "react-hook-form";
+import Order from './Order/Order';
+import { AgencyUser } from '../../../App';
+import ServiceList from './ServiceList/ServiceList';
+import { useParams } from 'react-router-dom';
+const Dashboard = () => {
+    
+    const [loginUser , setLoginUser] = useContext(AgencyUser);
+    
+    const value = sessionStorage.getItem('admin');
+    const {serviceTitle} = useParams();
+
+    return (
+        <section>
+           {(value==='false')? <Order></Order>:<ServiceList></ServiceList>}
+        </section>
+    );
+};
+
+export default Dashboard;
+
+// 
