6c068c423c8480b45b2ac148d3460c3dfce7e38b
Your NameMdTahsinAmin <Tahsin16@cse.pstu.ac.bd.com>
2020-10-19 10:53:27 +0600

    all api change

---
+++ .history/src/components/Dashboard/Dashboard/Order/Order_20201019104830.js
@@ -0,0 +1,115 @@
+import React, { useContext, useState } from 'react';
+import ExtraAppBar from '../../../Share/ExtraAppBar/ExtraAppBar';
+import SideBar from '../SideBar/SideBar';
+import './Order.css'
+import { useForm } from "react-hook-form";
+
+import cloudLogo from '../../../../images/cloud-upload-outline 1.png';
+import {AiOutlineCloudUpload}  from 'react-icons/ai'; 
+import { AgencyUser } from '../../../../App';
+import { useParams } from 'react-router-dom';
+const Order = () => {
+    
+    const {serviceTitle} = useParams();
+
+    const [fileName , setFileName] = useState('Upload image');
+
+    const [file ,setFile] = useState('');
+
+    const { register, handleSubmit, watch, errors } = useForm();
+
+    const [loginUser , setLoginUser] = useContext(AgencyUser);
+   
+    const [isSuccess , setSuccess] = useState('');
+
+    const onChange = (event) =>{
+         setFile(event.target.files[0]);
+         setFileName(event.target.files[0].name);
+    }
+
+
+    const onSubmit = (data) =>{
+      const formData = new FormData();
+
+      formData.append('name',data.name);
+
+      formData.append('email',data.email);
+
+      formData.append('course',data.course);
+
+      formData.append('projectDetails',data.projectDetails);
+
+      formData.append('file',file);
+
+      formData.append('status',false);
+
+      fetch('https://whispering-eyrie-37325.herokuapp.com/clientEnrollCourse',{
+            method: 'POST',
+            body:formData
+        })
+        .then(response => response.json())
+        .then(result => setSuccess(result));
+
+    }
+    
+
+    return (
+        <section className="container-fluid row">
+                <div>
+                <ExtraAppBar side={true}></ExtraAppBar>
+                <SideBar></SideBar>
+                </div>
+               <div className="col-md-9 order-place p-4 pl-5" >
+               <h5 className="text-brand mt-2">Order</h5>
+              
+              { (loginUser.name || loginUser.currentName) && <h5 style={{position:'absolute',right:0,top:'-33px'}}>{loginUser.name||loginUser.currentName}</h5>}
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                       
+                       <div className="row mt-4">
+                           <div className="col-md-12 order-section mb-3">
+
+                           <input type="text" name='name' defaultValue={loginUser.currentName||loginUser.name} className='form-control' ref={register} placeholder='Your name / company’s name'/>
+
+                           </div>
+
+                           <div className="col-md-12 order-section mb-3">
+                               
+                             <input type="email" name="email" defaultValue={loginUser.currentEmail||loginUser.email} className="form-control" ref={register({ required: true })} placeholder='Your email address'/> 
+
+                           </div>
+
+                           <div className="col-md-12 order-section mb-3">
+                               
+                               <input type="text" name="course" defaultValue={serviceTitle} className="form-control" ref={register({ required: true })} placeholder='Graphic Design'/>
+  
+                             </div>
+
+                             <div className="col-md-12 order-section mb-3">
+                               
+                              <textarea name="projectDetails" className="form-control" id="" cols="25" rows="5" ref={register} placeholder="Your message *"></textarea>
+  
+                             </div>
+
+                             <div className="col-md-4 servicePrice">
+
+                                <input className='form-control' name="price" placeholder='Price *' ref={register} />
+
+                             </div>
+                             <div className="col-md-4 order-label">
+                             <input type="file" name="image" id="file" ref={register} onChange={onChange}/>
+                                <label htmlFor="file">
+                                <AiOutlineCloudUpload className ='cloudLogo'/>
+                                  <span className='upload'> {fileName}</span></label>
+                             </div>
+                       </div>
+                       <input type="submit" value='Send' className='hireUs-btn mt-3'/>
+                    </form>
+                 
+             </div>
+
+        </section>
+    );
+};
+// <h5 className="text-brand mt-2">Order</h5>
+//{ loginUser.name && <h5 style={{position:'absolute',right:0,top:'33px'}}>{loginUser.name}</h5>}
+export default Order;
---
+++ .history/src/components/Dashboard/Dashboard/customerServiceList/CustomerServiceList_20201019104948.js
@@ -0,0 +1,61 @@
+import { CircularProgress, LinearProgress } from '@material-ui/core';
+import React, { useContext, useEffect, useState } from 'react';
+import { AgencyUser } from '../../../../App';
+import ExtraAppBar from '../../../Share/ExtraAppBar/ExtraAppBar';
+import SideBar from '../SideBar/SideBar';
+import CustomerServiceDetail from './CustomerServiceDetail';
+
+const CustomerServiceList = () => {
+     
+     const [loginUser,setLoginUser] = useContext(AgencyUser);
+     
+     const [customerService , setCustomerService] = useState([]);
+     
+
+     useEffect(()=>{
+           
+        async function loadData(){
+
+           const request =  await fetch(`https://whispering-eyrie-37325.herokuapp.com/clientEnrollServices?email=${(loginUser.email||loginUser.currentEmail)}`,{
+              method: 'GET',
+              headers: { 
+                'Content-Type': 'application/json',
+                authorization: `Bearer ${sessionStorage.getItem('token')}`
+              }
+           })
+
+           return request;
+        }
+       
+        loadData().then(response => response.json())
+        .then(data =>setCustomerService(data));
+
+     },[])
+     
+    
+    return (
+        <section className="container-fluid row">
+        <div>
+          <ExtraAppBar side={true}></ExtraAppBar>
+          <SideBar></SideBar>
+        </div>
+        <div className="col-md-9 p-4 pl-5 order-place">
+        <h5 className="text-brand mt-2">Service List</h5> 
+          { (loginUser.name || loginUser.currentName) && <h5 style={{position:'absolute',right:0,top:'-33px'}}>{loginUser.name||loginUser.currentName}</h5>}
+          <div className=''>
+             <div className="row">
+               
+               {  customerService.length === 0 ? <CircularProgress/>:
+                   customerService.map(service=><CustomerServiceDetail service={service} key={service._id}></CustomerServiceDetail>)
+               }
+                 
+             </div>
+
+          </div>
+
+        </div>
+    </section>
+    );
+};
+
+export default CustomerServiceList;
